b41d4e8c692153ff45c0a82e3fc93ad5de243a2e
TuanAnh <TuanAnh@192.168.5.101>
2021-11-03 21:33:25 +0700

    Ignore test_output

---
+++ webdriver/api/Topic_01_check_environment.java
@@ -0,0 +1,46 @@
+package api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_check_environment {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://tiki.vn/");
+	}
+
+	@Test
+	public void TC_01_ValidateCurrentUrl() {
+		// Login Page Url matching
+		String loginPageUrl = driver.getCurrentUrl();
+		Assert.assertEquals(loginPageUrl, "https://tiki.vn/");
+	}
+
+	@Test
+	public void TC_02_ValidatePageTitle() {
+		// Login Page title
+		String loginPageTitle = driver.getTitle();
+		Assert.assertEquals(loginPageTitle, "Tiki - Mua hàng online giá tốt, hàng chuẩn, ship nhanh");
+	}
+
+	
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
